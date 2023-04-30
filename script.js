cdea1028459b8f7bf8dd18f3d65407333aa36629
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-30 20:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const list = document.getElementById('list');
+const items = ['List Item 11', 'List Item 12'];
+
+function addItems() {
+  for (let i = 0; i < items.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = items[i];
+    list.appendChild(li);
+  }
+}
+
+addItems();
+
+list.addEventListener('scroll', () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems();
+  }
+});
 
