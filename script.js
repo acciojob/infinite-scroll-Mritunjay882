2fb7dd773f9cb4ebb7de82c87cd32461583e21e8
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-30 20:43:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your code here!
-const list = document.getElementById('list');
-const items = ['List Item 11', 'List Item 12'];
+const list = document.getElementById('infi-list');
+const items = ['infi-List Item 11', 'infi-List Item 12'];
 
 function addItems() {
   for (let i = 0; i < items.length; i++) {
